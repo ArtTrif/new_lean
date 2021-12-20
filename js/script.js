5c1d30423e2a37857c07e622fdec51b3931ab8bb
ArtTrif <arttrif@yandex.ru>
2021-12-20 17:18:13 +0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,23 @@
+"use strict";
+
+// let arr = [1, 2, 3, 4, 5, [5, 3, 1]];
+// console.log(arr[5],arr[3]);
+
+// alert('Hello');
+
+// const result = confirm("ты тут?");
+// console.log (result);
+
+// const answers = []; //создали пустой массив
+
+// // // далее заполняем массив
+// answers [0] = prompt('Как ваше Имя?', '');
+// answers [1] = prompt('Как ваше Фамилия?', '');
+// answers [2] = prompt('Как ваше Отчество?', '');
+
+
+// // //выводим в консоль
+// console.log (answers);
+
+const answer = prompt ("сколько вам лет?", "18");
+console.log(answer);
